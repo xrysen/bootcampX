734e2c48cc90836f0fa1157ed43a181f5ea88ce2
Sean Oyler <xrysen@gmail.com>
2020-11-23 22:16:25 +0000

    Adds queries for assignment durations

---
+++ 2_queries_joins/2_total_duration_per_cohort.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
